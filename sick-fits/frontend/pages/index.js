97626d50ec183f71ebd1ca9b204da3cc3bdffb22
hamish-w-smith <hamish.w.smith@gmail.com>
2020-01-06 07:41:05 +1300

    setting up App

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Home = () => (
-    <h1>hello</h1>
+    <>
+        <h1>Home page</h1>
+        <Link href="/sell">
+            <a>Sell page</a>
+        </Link>
+    </>
 )
 
 export default Home
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Sell = () => (
+    <>
+        <h1>Sell page</h1>
+        <Link href='/'>
+            <a>Home</a>
+        </Link>
+    </>
+)
+
+export default Sell
