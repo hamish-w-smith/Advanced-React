e3b2c6c2bd6e2b4f6bac07fd4337c2a2cf60e92e
hamish-w-smith <hamish.w.smith@gmail.com>
2020-01-08 07:36:22 +1300

    add Header

---
+++ sick-fits/frontend/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header(props) {
+  return (
+    <div>
+      <div className="bar">
+        <a href="">Floodsies</a>
+      </div>
+      <div className="sub-bar">
+        <p>Search</p>
+      </div>
+      <div>Cart</div>
+    </div>
+  );
+}
+
+export default Header;
--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
+import Header from "../components/Header";
 
 class Page extends Component {
   render() {
     return (
       <div>
+        <Header />
         <p>Hey I'm the Page component</p>
         {this.props.children}
       </div>
